c1f934ed107feead1ff7475895dd6f12ede80323
batmanov <batman@mail.ru>
2017-08-21 14:58:08 +0300

    mailer message-id

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
+  default "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{Rails.application.secrets[:host]}"
   layout 'mailer'
 end
