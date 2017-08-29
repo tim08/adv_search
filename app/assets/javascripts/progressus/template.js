267af2a7e393ac7b37934f4a319ca65d20c70563
batmanov <batman@mail.ru>
2017-08-29 09:48:59 +0300

    front fix

--- app/assets/javascripts/progressus/template.js
+++ app/assets/javascripts/progressus/template.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($) {
 
-	$(".headroom").headroom({
-		"tolerance": 20,
-		"offset": 50,
-		"classes": {
-			"initial": "animated",
-			"pinned": "slideDown",
-			"unpinned": "slideUp"
-		}
-	});
+	// $(".headroom").headroom({
+	// 	"tolerance": 5,
+	// 	"offset": 0,
+	// 	"classes": {
+	// 		"initial": "animated",
+	// 		"pinned": "slideDown",
+	// 		"unpinned": "slideUp"
+	// 	}
+	// });
 
 });
