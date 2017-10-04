d75eccfc2e83cbcda16a071bf0f50977d7cc168e
batmanov <batman@mail.ru>
2017-10-04 14:29:36 +0300

    fix bug

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
 
   def index
     @companies = Company.search(params)
-    @companies_news = Company.news_projects
+    @projects_news = Company.news_projects
   end
 
   def important_article
