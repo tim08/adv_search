a6087b6d7639af0f743f9c4ae57d1909aefb10cc
batmanov <batman@mail.ru>
2018-09-17 23:37:19 +0300

    resize project images

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
 
   validates :name, presence: true
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :logo, styles: {large: "900x600", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
   validates_attachment_size :logo, less_than: 5.megabytes
 end
