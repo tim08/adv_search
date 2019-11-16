11a0eb9d0499ae3c3c1b567fddd9eecb7506d643
batmanov <batman@mail.ru>
2019-11-16 15:52:01 +0300

    add v2 google recapcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LdtY8AUAAAAAAL2ZScQByfXtnwflmf7UZaElXwP'
-  config.secret_key = '6LdtY8AUAAAAADFnl-AtbF0np1f_kAZueuaxqigF'
+  config.site_key  = '6Le_8sIUAAAAAOfA7buYGnLDbFoRZf1l5YBfZ3XW'
+  config.secret_key = '6Le_8sIUAAAAAP0dKtPwkiWVcb9fgKbbsWBDVDRu'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
